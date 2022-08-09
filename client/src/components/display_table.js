6393e3f2c1d987584e075941bec81db99db2ddb0
Dlorej <jeroldlimjunpin@gmail.com>
2022-08-09 19:06:16 +0800

    first commit test heroku deploy

---
+++ client/src/components/display_table.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+const DisplayTable = () => {
+    const [info, setInfo] = useState([]);
+    const getInfo = async () => {
+        try {
+            const response = await fetch("/todos");
+            const responseData = await response.json();
+            setInfo(responseData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    useEffect(() => {
+        getTodos();
+    }, []);
+    return <div>JSON.stringify(info)</div>;
+};
+export default DisplayTable;
