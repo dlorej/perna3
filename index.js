bfe473854c0a48aca1a6c0c8cfc30187c0466cb6
Dlorej <jeroldlimjunpin@gmail.com>
2022-08-09 23:20:53 +0800

    fix errors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const pool = require("./db");
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,4 +22,8 @@ app.get("/getall", async (req, res) => {
     } catch (err) {
         console.error(err.message);
     }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is starting on port ${PORT}`);
 });
